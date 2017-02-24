40130168a5aa7d5e3e2d2c1f05c2dccd6c5ce796
Ethan Ferry <egferry@csbsju.edu>
2017-02-24 13:14:24 -0600

    AccountInterface and AdminInterface -ethan

---
+++ AccountInterface.java
@@ -0,0 +1,33 @@
+/*
+ * This is an abstract class that is interface for accounts
+ *
+ * @author Ethan Ferry
+ * @version 0.1
+ *
+ */
+
+
+public abstract class AccontInterface{
+	
+	/*
+ 	* This is a method to logout
+ 	*
+ 	*/
+
+	public void logout(){
+
+	}
+	
+	/*
+ 	* This is a method to log in
+ 	*
+ 	* @param username the username of the account logging in
+ 	* @param password the password of the account logging in
+	*/ 
+
+	public void login(String username, String password){
+
+	}
+
+
+}
