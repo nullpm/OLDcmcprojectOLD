3bf0b39f41aa01d78857b0b299de0aed542421c1
K1BECKER <k1becker@viewlincsci36.ad.csbsju.edu>
2017-02-26 14:35:54 -0600

    Made initial outline for these 3 classes

---
+++ AdminFuncController.java
@@ -0,0 +1,46 @@
+/**
+ * This is a class that controls the admin functions
+ * 
+ * @author Kyle Becker
+ * @version 0.1
+ */
+public class AdminFuncController{
+  
+ /**
+  * This method adds a university to the system
+  * 
+  * @param name of university
+  * @param location of university
+  * @param numberOfStudents at the university
+  * @param percentFemale students at the university
+  * @param SATVerbal of the average student at the university
+  * @param SATMath of the average student at the university
+  * @param expense of the university
+  * @param percentFinancialAid of the average student at the university
+  * @param numberOfApplicants at the university
+  * @param perecentAdmitted at the university
+  * @param percentEnrolled at the university
+  * @param academicScale of the university
+  * @param socialScale of the university
+  * @param empases of the university
+  * @return true if univerity added successfully
+  */
+ public boolean addUniversity(String name, String location, int numberOfStudents, int percentFemale, int SATVerbal, int SATMath, int expense, int percentFinancialAid, int numberOfApplicants, int perecentAdmitted, int percentEnrolled, int academicScale, int socialScale, String empases){
+  return false;
+ }
+ 
+   /**
+   * This method adds an account to the system
+   * 
+   * @param firstname the fistname of the user represented by the account
+   * @param lastname the lastname of the user represented by the account
+   * @param username the username of the user represented by the account
+   * @param password the password of the user represented by the account
+   * @param type the account type of the user represented by the account
+   * @param status the account status of the user represented by the account
+   * @returns true if account added successfully, otherwise false
+   */
+  public boolean addUser(String firstname, String lastname, String username, String password, Char type, Char status){
+    return true;
+  }
+}
